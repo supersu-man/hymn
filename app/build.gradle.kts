316a5f4e7bb20e826164e593781ff286ba96ffdf
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2024-07-17 09:26:21 +0530

    deps: newPipExtractor, androidx

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,16 +36,16 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.github.supersu-man:apkupdater-library:v2.1.0")
-    implementation("com.github.teamnewpipe:NewPipeExtractor:v0.24.0")
+    implementation("com.github.teamnewpipe:NewPipeExtractor:v0.24.1")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("androidx.media3:media3-exoplayer:1.3.1")
     implementation("androidx.media3:media3-session:1.3.1")
-    implementation("androidx.activity:activity:1.8.0")
+    implementation("androidx.activity:activity:1.9.0")
 }
